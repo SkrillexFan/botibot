2fc5afe91382a3e50e3a390f28f2d2db807fc75a
omarmadeinchina <omarmahir11@gmail.com>
2020-03-20 04:37:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,23 +13,7 @@ client.on("ready", function() {
 });
 
 client.on("ready",function(){
-    while(true)
-    {
-        setTimeout(function () {
-            client.user.setActivity("with babak");
-          }, 10000);
-          setTimeout(function () {
-            client.user.setActivity("with khtk");
-          }, 10000);
-          setTimeout(function () {
-            client.user.setActivity("with mamak");
-          }, 10000);
-          setTimeout(function () {
-            client.user.setActivity("with khok");
-          }, 10000);
-    }
-     
-    
+            client.user.setActivity("with mok");
 })
  
 client.on("message", function(message) {
